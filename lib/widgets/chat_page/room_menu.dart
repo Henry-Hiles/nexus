cafdf43fd3a66a99f644ab49ef094405ba7b86b6
Henry-Hiles <henry@henryhiles.com>
2026-01-30 19:04:03 +0100

    removing unused imports because they annoy me

--- lib/widgets/chat_page/room_menu.dart
+++ lib/widgets/chat_page/room_menu.dart
@@ -1,11 +1,8 @@
 import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:flutter/material.dart";
-import "package:flutter/services.dart";
-import "package:flutter_hooks/flutter_hooks.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:nexus/controllers/client_controller.dart";
 import "package:nexus/models/room.dart";
-import "package:nexus/widgets/form_text_input.dart";
 
 class RoomMenu extends ConsumerWidget {
   final Room room;
