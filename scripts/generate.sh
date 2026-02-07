c3c3863aadc85e4fc7c3d495b58e09312f4845c2
Henry-Hiles <henry@henryhiles.com>
2026-02-06 19:15:13 -0500

    pipe push/popd to dev null

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-pushd "$(dirname "$(readlink -f "$0")")"/.. || exit
+pushd "$(dirname "$(readlink -f "$0")")"/.. > /dev/null || exit
 
 mkdir -p build
 touch build/lock
 dart scripts/generate.dart
 rm build/lock
 
-popd || exit 
+popd > /dev/null || exit 
