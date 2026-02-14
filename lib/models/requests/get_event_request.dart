b88f636e34e25d1e5f288974b2f65a18a49eed14
Henry-Hiles <henry@henryhiles.com>
2026-02-13 21:54:29 -0500

    fix get event request json

--- lib/models/requests/get_event_request.dart
+++ lib/models/requests/get_event_request.dart
@@ -3,13 +3,20 @@ import "package:nexus/models/room.dart";
 part "get_event_request.freezed.dart";
 part "get_event_request.g.dart";
 
-@freezed
+@Freezed(toJson: false)
 abstract class GetEventRequest with _$GetEventRequest {
+  const GetEventRequest._();
   const factory GetEventRequest({
     required Room room,
     required String eventId,
     @Default(false) bool unredact,
   }) = _GetEventRequest;
+
+  Map<String, dynamic> toJson() => {
+    "room_id": room.metadata?.id,
+    "event_id": eventId,
+    "unredact": unredact,
+  };
 
   factory GetEventRequest.fromJson(Map<String, Object?> json) =>
       _$GetEventRequestFromJson(json);
