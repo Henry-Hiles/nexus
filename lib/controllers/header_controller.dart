ae86d58025bee2329d5a6ed69558db8a56efc1e3
Henry-Hiles <henry@henryhiles.com>
2026-03-06 18:28:56 -0500

    listen to client state in header controller

--- lib/controllers/header_controller.dart
+++ lib/controllers/header_controller.dart
@@ -1,9 +1,13 @@
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:nexus/controllers/client_controller.dart";
+import "package:nexus/controllers/client_state_controller.dart";
 
 class HeaderController extends AsyncNotifier<Map<String, String>> {
   @override
   Future<Map<String, String>> build() async {
+    if (ref.watch(ClientStateController.provider)?.isLoggedIn != true) {
+      return {};
+    }
     final client = ref.watch(ClientController.provider.notifier);
     final accessToken = await client.getAccessToken();
     return {"authorization": "Bearer $accessToken"};
