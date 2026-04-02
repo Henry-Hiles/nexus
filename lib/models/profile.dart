e30355a6f10b4165af4f56d019c78392f5e478f0
Henry-Hiles <henry@henryhiles.com>
2026-04-02 10:58:34 -0400

    Support stable identifiers for MSC4175

--- lib/models/profile.dart
+++ lib/models/profile.dart
@@ -3,15 +3,22 @@ import "package:freezed_annotation/freezed_annotation.dart";
 part "profile.freezed.dart";
 part "profile.g.dart";
 
+Object? readPronouns(Map<dynamic, dynamic> map, _) =>
+    map["m.pronouns"] ?? map["io.fsky.nyx.pronouns"];
+
+Object? readTimezone(Map<dynamic, dynamic> map, _) =>
+    map["m.tz"] ?? map["us.cloke.msc4175.tz"];
+
 @freezed
 abstract class Profile with _$Profile {
   const factory Profile({
     String? avatarUrl,
     @JsonKey(name: "displayname") String? displayName,
-    @JsonKey(name: "us.cloke.msc4175.tz") String? timezone,
+
+    @JsonKey(readValue: readTimezone) String? timezone,
 
     @Default(IList.empty())
-    @JsonKey(name: "io.fsky.nyx.pronouns")
+    @JsonKey(readValue: readPronouns)
     IList<Pronoun> pronouns,
   }) = _Profile;
 
