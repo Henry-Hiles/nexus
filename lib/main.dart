eecc307f0438f044405a080bbf239f7202c7f67e
Henry-Hiles <henry@henryhiles.com>
2026-07-22 16:35:18 -0400

    disable provider auto retry

--- lib/main.dart
+++ lib/main.dart
@@ -84,7 +84,7 @@ void main() async {
 
   runApp(
     ProviderScope(
-      retry: null,
+      retry: (_, _) => null,
       observers: [
         // Change false to true if you want debug information on provider reloads
         // ignore: dead_code
