5f13d3fbf5cb76872aa95b3d2918addf087a8168
Henry-Hiles <henry@henryhiles.com>
2026-07-27 22:02:20 -0400

    don't show errors when url preview fails

--- lib/controllers/url_preview.dart
+++ lib/controllers/url_preview.dart
@@ -1,3 +1,4 @@
+import "package:flutter/widgets.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:nexus/controllers/client.dart";
 import "package:nexus/models/open_graph_data.dart";
@@ -10,9 +11,14 @@ class UrlPreviewController extends AsyncNotifier<OpenGraphData?> {
   Future<OpenGraphData?> build() async {
     if (url.host == "matrix.to") return null;
 
-    return await ref
-        .watch(ClientController.provider.notifier)
-        .getUrlPreview(url);
+    try {
+      return await ref
+          .watch(ClientController.provider.notifier)
+          .getUrlPreview(url);
+    } catch (error, stackTrace) {
+      debugPrintStack(label: error.toString(), stackTrace: stackTrace);
+      return null;
+    }
   }
 
   static final provider =
