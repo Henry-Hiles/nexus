0a667fda0bc99c8d725c4d4c0175caab6bcb6eee
Henry-Hiles <henry@henryhiles.com>
2026-09-09 15:44:25 -0400

    update gomuks vendorHash

--- linux/nix/pkg/gomuks.nix
+++ linux/nix/pkg/gomuks.nix
@@ -17,7 +17,7 @@ buildGoModule (
 
     src = "${src}/gomuks";
 
-    vendorHash = "sha256-C03ss88QAnmZu+XxoHhc1M/261xFl8hR/pzLbU37Qe8=";
+    vendorHash = "sha256-9CS/lAVT8gLWACLomwcEa8YNlAXvBOskPPx4zERg0wQ=";
 
     buildPhase = ''
       runHook preBuild
