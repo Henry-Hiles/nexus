031fbe326cb4bbc3788b4cfdf580e34aac865a97
Henry-Hiles <henry@henryhiles.com>
2026-09-26 13:42:31 -0400

    change highlight timing

--- lib/widgets/room_chat/room_chat.dart
+++ lib/widgets/room_chat/room_chat.dart
@@ -100,11 +100,10 @@ final class const RoomChat({
     Future<void> jumpToId(String eventId) async {
       highlightedEvent.value = eventId;
 
-      final jump = scroll.jumpToId(eventId);
-      await Future.delayed(.new(seconds: 1), () {
+      await scroll.jumpToId(eventId);
+      await Future.delayed(.new(milliseconds: 700), () {
         if (highlightedEvent.value == eventId) highlightedEvent.value = null;
       });
-      await jump;
     }
 
     IList<PopupMenuEntry> getEventOptions(Event event) =>
